2ebafc03113b491bb42400baf86eb599472f0775
Петър Ангелов Димитров <padimitrov23@codingburgas.bg>
2025-08-01 00:04:48 +0300

    First commit

---
+++ main_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var button_type = null
+
+func _on_start_button_pressed() -> void:
+	print("start pressed")
+	button_type = "start"
+	$Transition.show()
+	$Transition/FadeTimer.start()
+	$Transition/AnimationPlayer.play("fade_in")   
+
+
+func _on_options_button_pressed() -> void:
+	print("options pressed")
+	button_type = "options"
+	$Transition.show()
+	$Transition/FadeTimer.start()
+	$Transition/AnimationPlayer.play("fade_in") 
+
+
+func _on_quit_button_pressed() -> void:
+	print("quit pressed")  
+	get_tree().quit()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start" :
+		get_tree().change_scene_to_file("res://game.tscn")
+		
+	elif button_type == "options" :
+		get_tree().change_scene_to_file("res://options.tscn")
